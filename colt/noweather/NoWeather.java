ea9c455ac989cc16b40ac890b1025a72ec1891bb
Colt <zcewlt@gmail.com>
2016-10-23 14:41:58 -0400

    Create NoWeather.java

---
+++ colt/noweather/NoWeather.java
@@ -0,0 +1,30 @@
+package colt.noweather;
+
+import java.util.Collection;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.weather.WeatherChangeEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NoWeather extends JavaPlugin implements Listener {
+	private Collection<String> worlds;
+
+	public void onEnable() {
+		Bukkit.getPluginManager().registerEvents(this, this);
+		saveDefaultConfig();
+		this.worlds = getConfig().getStringList("worlds");
+	}
+
+	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+	public void WeatherChangeEvent(WeatherChangeEvent event) {
+		if (!event.toWeatherState()) return;
+		if (this.worlds.contains(event.getWorld().getName())) {
+			event.setCancelled(true);
+			event.getWorld().setWeatherDuration(0);
+			event.getWorld().setThundering(false);
+		}
+	}
+}
